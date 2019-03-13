35067356ea40a4ac95779cfa557fd0507e5a1a80
Eddie <willett.eddie@gmail.com>
2019-03-12 23:10:31 -0400

    fix jekyll

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -146,9 +146,5 @@ fi
 
 if [ $JEKYLL = "true" ] ; then
     echo "RUN apt-get install ruby-full build-essential zlib1g-dev -y \
-    && echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc \
-    && echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc \
-    && echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc \
-    && source ~/.bashrc \
     && gem install jekyll bundler"
 fi
