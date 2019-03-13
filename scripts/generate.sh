899092f87e8da0199711eb046fd39fd6790bcae7
Eddie <willett.eddie@gmail.com>
2019-03-12 22:26:13 -0400

    add jekyll and boto

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -139,3 +139,16 @@ if [ $ANSIBLE = "true" ] ; then
     && apt-get update -y \
     && apt-get install ansible -y "
 fi
+
+if [ $BOTO= "true" ] ; then
+    echo "RUN apt-get install python3-boto3 -y"
+fi
+
+if [ $JEKYLL= "true" ] ; then
+    echo "RUN apt-get install ruby-full build-essential zlib1g-dev -y \
+    && echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc \
+    && echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc \
+    && echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc \
+    && source ~/.bashrc \
+    && gem install jekyll bundler"
+fi
