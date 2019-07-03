f86be2373dc70132733e2fdf627431ed63a03667
Eddie <willett.eddie@gmail.com>
2019-07-03 15:42:08 -0400

    no java

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -39,32 +39,32 @@ fi
 #     wget "http://php.net/distributions/php-${PHP_VERSION_NUM}.tar.xz"
 # fi
 
-if [ $JAVA = "true" ] ; then
-cat << EOF
-RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
-    echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list && \\
-    apt-get update && apt-get -y install -t jessie-backports openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
-		apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
-; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
-		apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
-    apt-get update && \\
-    apt-get --force-yes -y install software-properties-common python-software-properties && \\
-    echo | add-apt-repository -y ppa:webupd8team/java && \\
-    apt-get update && \\
-    echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
-    apt-get -y install oracle-java8-installer \\
-; fi
-EOF
-fi
+# if [ $JAVA = "true" ] ; then
+# cat << EOF
+# RUN if [ \$(grep 'VERSION_ID="8"' /etc/os-release) ] ; then \\
+#     echo "deb http://ftp.debian.org/debian main" >> /etc/apt/sources.list && \\
+#     apt-get update && apt-get -y install -t openjdk-8-jdk ca-certificates-java \\
+# ; elif [ \$(grep 'VERSION_ID="9"' /etc/os-release) ] ; then \\
+# 		apt-get update && apt-get -y -q --no-install-recommends install -t stable openjdk-8-jdk ca-certificates-java \\
+# ; elif [ \$(grep 'VERSION_ID="14.04"' /etc/os-release) ] ; then \\
+# 		apt-get update && \\
+#     apt-get --force-yes -y install software-properties-common python-software-properties && \\
+#     echo | add-apt-repository -y ppa:webupd8team/java && \\
+#     apt-get update && \\
+#     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
+#     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
+#     apt-get -y install oracle-java8-installer \\
+# ; elif [ \$(grep 'VERSION_ID="16.04"' /etc/os-release) ] ; then \\
+#     apt-get update && \\
+#     apt-get --force-yes -y install software-properties-common python-software-properties && \\
+#     echo | add-apt-repository -y ppa:webupd8team/java && \\
+#     apt-get update && \\
+#     echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \\
+#     echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections && \\
+#     apt-get -y install oracle-java8-installer \\
+# ; fi
+# EOF
+# fi
 
 if [ $MYSQL_CLIENT = "true" ] ; then
     echo "RUN apt-get -y install mysql-client"
@@ -162,5 +162,5 @@ ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US.UTF-8"
 
 if [ $SELENIUM = "true" ] ; then
-    echo "RUN pip3 install selenium" 
+    echo "RUN pip3 install selenium"
 fi
